f63f1497a73e3cd71bfbd15693c876c841c23c79
Souhardya-27 <sahasouhardya1@gmail.com>
2026-01-16 23:13:06 +0530

    Add files via upload

---
+++ recommended_movie_names/main.py
@@ -0,0 +1,75 @@
+import pickle
+import streamlit as st
+import requests
+import pandas as pd
+
+# -------------------- Fetch poster --------------------
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    data = requests.get(url).json()
+    poster_path = data.get('poster_path')
+    if poster_path:
+        return "https://image.tmdb.org/t/p/w500/" + poster_path
+    return ""
+
+# -------------------- Recommendation logic --------------------
+def recommend(movie):
+    if movie not in movies['title'].values:
+        return [], []
+
+    index = movies[movies['title'] == movie].index[0]
+
+    distances = sorted(
+        list(enumerate(similarity[index])),
+        reverse=True,
+        key=lambda x: x[1]
+    )[1:6]
+
+    recommended_movie_names = []
+    recommended_movie_posters = []
+
+    for i in distances:
+        movie_id = movies.iloc[i[0]]['id']   # ✅ FIX HERE
+        recommended_movie_names.append(movies.iloc[i[0]]['title'])
+        recommended_movie_posters.append(fetch_poster(movie_id))
+
+    return recommended_movie_names, recommended_movie_posters
+
+# -------------------- Streamlit UI --------------------
+st.header('Movie Recommender System')
+
+#Load movies correctly as DataFrame
+movie_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+movies = pd.DataFrame(movie_dict)
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+movie_list = movies['title'].values
+selected_movie = st.selectbox(
+    "Type or select a movie from the dropdown",
+    movie_list
+)
+
+if st.button('Show Recommendation'):
+    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    with col1:
+        st.text(recommended_movie_names[0])
+        st.image(recommended_movie_posters[0])
+
+    with col2:
+        st.text(recommended_movie_names[1])
+        st.image(recommended_movie_posters[1])
+
+    with col3:
+        st.text(recommended_movie_names[2])
+        st.image(recommended_movie_posters[2])
+
+    with col4:
+        st.text(recommended_movie_names[3])
+        st.image(recommended_movie_posters[3])
+
+    with col5:
+        st.text(recommended_movie_names[4])
+        st.image(recommended_movie_posters[4])
